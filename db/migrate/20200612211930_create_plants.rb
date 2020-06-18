5da6a90460cf73b63863df079f4cce41423e9a05
William <willlin1996@gmail.com>
2020-06-18 10:32:40 -0400

    plots

--- db/migrate/20200612211930_create_plants.rb
+++ db/migrate/20200612211930_create_plants.rb
@@ -4,7 +4,6 @@ class CreatePlants < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :image
       t.string :image2
-      t.string :image3
       t.float :price
       t.float :sellprice
       t.string :category
